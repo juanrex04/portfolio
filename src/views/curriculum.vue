dc2ca1760558ebce3d3d456dcb1b04376f50b273
Juan Manuel <juanpix2404@gmail.com>
2020-10-03 15:53:56 -0500

    Esperar hacer un deploy en un server y verificar si sirve

--- src/views/curriculum.vue
+++ src/views/curriculum.vue
@@ -3,13 +3,13 @@
     <h1>Curriculum</h1>
     <img id="cv" src="../assets/img/cv.png" />
     <br />
-    <a href="https://drive.google.com/file/d/1ohYQnJLvUnMOMwNqCOROiXHduZUKJ-uG/view?usp=sharing" target="_BLANK">
+    <a href='../assets/docs/cv-juan.pdf' download="Juan Restrepo - CV.pdf">
       <b-button>Presiona aqui si quieres ver mi Curriculum Vitae</b-button>
     </a>
   </div>
 </template>
 <script>
-import pdfFile from '../assets/img/photo.jpg'
+//import pdfFile from '../assets/docs/cv-juan.pdf'
 //target="_BLANK"
 export default {};
 </script>
